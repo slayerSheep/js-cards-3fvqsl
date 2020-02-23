b940b27d12d3e520d3335772e6b7ceef98b73cca
slayerSheep <neilraghav@gmail.com>
2020-02-23 22:52:18 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-cards-3fvqsl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-cards-3fvqsl)
---
+++ index.js
@@ -0,0 +1,47 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+class Deck{
+  constructor(){
+    this.deck = [];
+    this.reset();
+    this.shuffle();
+  }
+
+  reset(){
+    this.deck = [];
+
+    const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+    const values = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+    for (let suit in suits) {
+      for (let value in values) {
+        		var card = {Value: values[value], Suit: suits[suit]};
+			      this.deck.push(card);
+      }
+    }
+  }
+
+  shuffle(){
+    const { deck } = this;
+    let m = deck.length, i;
+
+    while(m){
+      i = Math.floor(Math.random() * m--);
+
+      [deck[m], deck[i]] = [deck[i], deck[m]];
+    }
+
+    return this;
+  }
+
+  deal(){
+    return this.deck.pop();
+  }
+}
+
+const deck1 = new Deck();
+console.log(deck1.deck);
+deck1.reset();
+console.log(deck1.deck[0]);
